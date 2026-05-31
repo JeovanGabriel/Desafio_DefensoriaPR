4634f5eabeb7d4908148428668fc5d1c2f085daa
JeovanGabriel <jeovang.s.goncalves@gmail.com>
2026-05-31 17:36:31 -0300

    adcionando requisição

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 import requests
+import json
 
 app = FastAPI()
+parametro = {"Data Atual":"valor_data"}
+requisicao = requests.get("https://testedefensoriapr.pythonanywhere.com/precos", params=parametro)
 
 @app.get("/")
 def home():
-    return "Insira a data de hoje na url ao lado da / (Ex: DD/MM/AA)"
+    return "Insira a data de hoje na url ao lado da / (Ex: DD-MM-AA)"
+
+@app.get("/{data}")
+def pegar_data(data: str):
+    return requisicao.json()
