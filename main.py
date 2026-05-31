c2f1133501371d51e7ca7f1d46a947f1bff26304
JeovanGabriel <jeovang.s.goncalves@gmail.com>
2026-05-31 18:42:41 -0300

    Adcionando comentários

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+#Instalando as bibliotecas
 from fastapi import FastAPI
 import requests
 
+#Criand a API e a requisição da URL
 app = FastAPI()
-parametro = {"Data Atual":"valor_data"}
 requisicao = requests.get("https://testedefensoriapr.pythonanywhere.com/precos")
 
+#Texto a mostra na HomePage
 @app.get("/")
 def home():
     return "Insira a data de hoje na url ao lado da / (Ex: DD-MM-AA)"
 
+#Parametro data
 @app.get("/{data}")
 def pegar_data(data: str):
-    if requisicao.status_code == 200:
-        return {"Data Atual" : data, "Preços" : requisicao.json()}
-    else:
+    if requisicao.status_code == 200: #Caso o Satus-Code fique em 200 (Indicando que houve contato)
+        return {"Data Atual Informada" : data, "Preços" : requisicao.json()}
+    else: #Caso o Status-Code seja diferente de 200
         return "Infelizmente, não conseguimos encontrar os preços, por favor volte mais tarde"
